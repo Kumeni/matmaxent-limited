b3a5e5eba9e61c36f11e24207a5f54e5d37ba5af
samson kumeni <samsonkumeni8@gmail.com>
2022-04-03 06:29:55 +0300

    navigation links update

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import styles from './Navigation.module.css';
 import {useRouter} from 'next/router';
+import Link from "next/link";
 
 function Navigation(props) {
 
@@ -11,21 +12,39 @@ function Navigation(props) {
             {
                 router !== undefined && router.pathname !== "/" &&
                     <ul>
-                        <a href = "#">Homepage</a>
+                        <Link href="/">
+                            <a>Homepage</a>
+                        </Link>
                     </ul>
             }
             <ul className = {styles.ourServices}>
                 <li>Our services</li>
-                <a href = "#">Service1</a>
-                <a href = "#">Service2</a>
-                <a href = "#">Service3</a>
-                <a href = "#">Service4</a>
-                <a href = "#">Service5</a>
+                <Link href="/services/service-name">
+                    <a>Service1</a>
+                </Link>
+                <Link href="/services/service-name">
+                    <a>Service2</a>
+                </Link>
+                <Link href="/services/service-name">
+                    <a>Service3</a>
+                </Link>
+                <Link href="/services/service-name">
+                    <a>Service4</a>
+                </Link>
+                <Link href="/services/service-name">
+                    <a>Service5</a>
+                </Link>
             </ul>
             <ul>
-                <a href = "#">Projects</a>
-                <a href = "#">About us</a>
-                <a href = "#">Contact us</a>
+                <Link href="/projects">
+                    <a>Projects</a>
+                </Link>
+                <Link href="/about_us">
+                    <a>About us</a>
+                </Link>
+                <Link href="/contact_us">
+                    <a>Contact us</a>
+                </Link>
             </ul>
         </div>
     )
